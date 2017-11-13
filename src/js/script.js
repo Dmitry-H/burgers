d87f740224a443cf7fc1d43f08afbfac36bb3cc3
Dmitry Hays <haysdmitry@gmail.com>
2017-11-13 19:40:17 +0400

    Поправлено всплывающее меню

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,13 @@
 window.onload = function() {
     document.getElementById("menu-open").addEventListener("click", showMenu);
     document.getElementById("menu-close").addEventListener("click", hideMenu);
+    document.getElementById("composition-open").addEventListener("mouseenter", showComposition);
+    document.getElementById("composition-open").addEventListener("mouseleave", hideComposition);
+    /*    window.addEventListener("keypress", function (e) {
+        if (e.keyCode === 27) {
+            hideMenu(e);
+        }
+    });*/
 };
 
 function showMenu(e) {
@@ -14,4 +21,14 @@ function showMenu(e) {
 function hideMenu(e) {
     e.preventDefault();
     document.getElementById("menu").classList.remove("main-wrapper__hamburger-menu--visible");
+}
+
+function showComposition(e) {
+    document.getElementById("composition-open").classList.add("composition--active");
+    document.getElementById("composition-details").classList.add("composition-details--visible");
+}
+
+function hideComposition(e) {
+    document.getElementById("composition-open").classList.remove("composition--active");
+    document.getElementById("composition-details").classList.remove("composition-details--visible");
 }
